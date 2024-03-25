7059a36dc444ee7656fb5b069817fac08fde31fc
sagetony <uchetony224@gmail.com>
2024-03-25 11:35:38 +0100

    changes on YardNFTWrapper

--- src/contracts/interfaces/IYardNFTWrapper.sol
+++ src/contracts/interfaces/IYardNFTWrapper.sol
@@ -4,27 +4,33 @@ pragma solidity 0.8.20;
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 /**
-* @title IYardNFTWrapper
-* @author fps (@0xfps).
-* @dev Interface for interacting with Yard NFT Wrapper.
-* @notice   The Yard NFT Wrapper has a singular function of wrapping NFTs
-*           by keeping their URIs intact and sending that to specific addresses.
-*           Override ERC-721 `approve()`.
-*           Override ERc-721 `setApprovalForAll()`.
-*           Override ERC-721 `tokenURI().`
-*           Override ERC-721 `transferFrom()`.
-*           Override ERC-721 `safeTransferFrom()`.
-*           Override ERC-721 `safeTransferFrom()`.
-*           NFTs cannot be transferred until they're `release()`d.
-*           All functions here except `getOwner()` will be callable only by the `YardPair`.
-*/
+ * @title IYardNFTWrapper
+ * @author fps (@0xfps).
+ * @dev Interface for interacting with Yard NFT Wrapper.
+ * @notice   The Yard NFT Wrapper has a singular function of wrapping NFTs
+ *           by keeping their URIs intact and sending that to specific addresses.
+ *           Override ERC-721 `approve()`.
+ *           Override ERc-721 `setApprovalForAll()`.
+ *           Override ERC-721 `tokenURI().`
+ *           Override ERC-721 `transferFrom()`.
+ *           Override ERC-721 `safeTransferFrom()`.
+ *           Override ERC-721 `safeTransferFrom()`.
+ *           NFTs cannot be transferred until they're `release()`d.
+ *           All functions here except `getOwner()` will be callable only by the `YardPair`.
+ */
 
 interface IYardNFTWrapper {
     event Wrapped(uint256 indexed id, address indexed to);
     event Unwrapped(uint256 indexed id);
 
     function release(uint256 id) external;
-    function wrap(IERC721 nft, uint256 id, address to) external returns (uint256);
+
+    function wrap(
+        IERC721 nft,
+        uint256 id,
+        address to
+    ) external returns (uint256);
+
     function unwrap(uint256 id) external returns (bool);
 
     function isReleased(uint256 id) external view returns (bool);
