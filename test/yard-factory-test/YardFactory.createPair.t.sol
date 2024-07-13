7f0c3f1418819add68c38e555bbd563fdb3af7ae
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-07-13 17:04:59 +0100

    🧪 Isolate `testOnERC721Received`.

--- test/yard-factory-test/YardFactory.createPair.t.sol
+++ test/yard-factory-test/YardFactory.createPair.t.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import { IERC721Receiver } from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 import "./YardFactory.t.sol";
 
@@ -311,17 +310,6 @@ contract YardFactoryCreatePairTest is YardFactoryTest {
             address(feeToken),
             address(yardNFTWrapper),
             dick
-        );
-    }
-
-    function testOnERC721Received() public {
-        assertTrue(
-            yardFactory.onERC721Received(
-                address(testNFTA),
-                address(testNFTB),
-                block.timestamp,
-                bytes("YardFactory")
-            ) == IERC721Receiver.onERC721Received.selector
         );
     }
 }
