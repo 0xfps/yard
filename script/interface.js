1048d7eb980d12add6da9fd37744423259df841d
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-09-02 21:24:21 +0100

    chore: extract interfaces for `YardFactory` and `YardRouter`.

---
+++ script/interface.js
@@ -0,0 +1,29 @@
+const { ethers } = require("hardhat")
+const { readFileSync, existsSync, writeFileSync, mkdirSync } = require("fs")
+const path = require("path")
+
+const destinationDir = path.join(__dirname, "/config")
+const destination = path.join(__dirname, "/config/abis.json")
+
+if (!existsSync(destinationDir)) {
+    mkdirSync(destinationDir)
+}
+
+async function main() {
+    let abis = existsSync(destination) ? JSON.parse(readFileSync(destination)) : {}
+    const [yardFactory, yardRouter] = [await ethers.getContractFactory("YardFactory"), await ethers.getContractFactory("YardRouter")]
+
+    abis = {
+        ...abis,
+        factory: yardFactory.interface.fragments,
+        router: yardRouter.interface.fragments
+    }
+
+    try {
+        writeFileSync(destination, JSON.stringify(abis))
+    } catch (e) {
+        throw new Error(e)
+    }
+}
+
+main()
